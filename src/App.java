73897fef2e13d1d4b13b7f98027f655eeca7dd5c
Dhanushkumar-S-G <dhanushkumarganapathy@gmail.com>
2021-09-29 16:21:28 +0530

    establishment of connection

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.sql.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        test.connect();
+        
+    }
+}
+class test {
+    public static void  connect () throws Exception{
+        Connection con = null;            
+        Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("Connecting..");
+        //con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/", "root", "");
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "toor");
+        System.out.println("Connected");
+        Statement stmt=con.createStatement();  
+        stmt.executeQuery("USE university;");
+        ResultSet rs = stmt.executeQuery("SELECT * FROM course;");
+        while(rs.next())  
+            System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)+"  "+rs.getString(4));
+        con.close();
+
     }
 }
