63fa47513f34d62bcdac0d76edfd9e284ec95882
Dhanushkumar-S-G <dhanushkumarganapathy@gmail.com>
2021-09-29 19:26:36 +0530

    returning number of columns

--- src/App.java
+++ src/App.java
@@ -9,18 +9,25 @@ public class App {
 }
 class test {
     public static void  connect () throws Exception{
+        int col;
+        String cmd = "",c;
         Connection con = null;            
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName("com.mysql.jdbc.Driver"); // loading the driver
         System.out.println("Connecting..");
         //con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/", "root", "");
-        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "toor");
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "toor"); // establishing the connection
         System.out.println("Connected");
-        Statement stmt=con.createStatement();  
-        stmt.executeQuery("USE university;");
-        ResultSet rs = stmt.executeQuery("SELECT * FROM course;");
-        while(rs.next())  
-            System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)+"  "+rs.getString(4));
-        con.close();
+        Statement stmt=con.createStatement();  // creating statemtent object
+        stmt.executeQuery("USE billing;");
+        // information_schema provides access to database metadata
+        ResultSet rs = stmt.executeQuery("SELECT COUNT(COLUMN_NAME) FROM information_schema.columns WHERE table_name = 'product' "); //storing the resultant relation in resultset
+        rs.next();
+        col = rs.getInt(1);
+        for(int i = 1; i<=col;i++){
+            c = String.format("rs.getString(%d) + \" \"", i);
+            cmd += c;
+        }
+        System.out.println(cmd);
 
     }
 }
